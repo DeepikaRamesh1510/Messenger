e6f879145a0d72aa12cca9145136a8825a3dc98b
Deepika <deepikardb@gmail.com>
2023-06-26 22:28:11 -0700

    Main updates

--- Messenger/Main/AppDelegate.swift
+++ Messenger/Main/AppDelegate.swift
@@ -13,7 +13,10 @@ import UserDefaultsService
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+	func application(
+		_ application: UIApplication,
+		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+	) -> Bool {
 		
 		UserDefaultsService.instantiate(withSuiteName: AppConstants.suiteName)
 		// Override point for customization after application launch.
@@ -22,22 +25,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	// MARK: UISceneSession Lifecycle
 
-	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+	func application(
+		_ application: UIApplication,
+		configurationForConnecting connectingSceneSession: UISceneSession,
+		options: UIScene.ConnectionOptions
+	) -> UISceneConfiguration {
 		// Called when a new scene session is being created.
 		// Use this method to select a configuration to create the new scene with.
-		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+		return UISceneConfiguration(
+			name: "Default Configuration",
+			sessionRole: connectingSceneSession.role
+		)
 	}
 
-	func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+	func application(
+		_ application: UIApplication,
+		didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+	) {
 		// Called when the user discards a scene session.
 		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
 		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 	}
-
-
-}
-
-struct AppConstants {
-	static var bundleId: String { Bundle.main.bundleIdentifier! }
-	static var suiteName: String { bundleId + "_suit_name" }
 }
